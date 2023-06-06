424a235a3a3b3ac6ab52dbe28bce2a08d6b068d7
surajkabirath <surajkabirath1234@gmail.com>
2023-06-06 12:45:30 +0545

    blog page created

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import styles from "./page.module.css"
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <>
+    <Link href={`/blog/testId`} className={styles.container} >
+      <div className={styles.imageContainer}>
+        <Image
+          src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+          alt=""
+          width={400}
+          height={250}
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>PhotoGraphy</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+          itaque voluptatibus qui voluptatum tempore delectus nemo accusantium
+          ex quisquam doloribus inventore nihil perferendis quia iure eos fugit
+          quod est facere?
+        </p>
+      </div>
+    </Link>
+    <Link href={`/blog/testId`} className={styles.container} >
+      <div className={styles.imageContainer}>
+        <Image
+          src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+          alt=""
+          width={400}
+          height={250}
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>PhotoGraphy</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+          itaque voluptatibus qui voluptatum tempore delectus nemo accusantium
+          ex quisquam doloribus inventore nihil perferendis quia iure eos fugit
+          quod est facere?
+        </p>
+      </div>
+    </Link>
+    <Link href={`/blog/testId`} className={styles.container} >
+      <div className={styles.imageContainer}>
+        <Image
+          src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+          alt=""
+          width={400}
+          height={250}
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>PhotoGraphy</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+          itaque voluptatibus qui voluptatum tempore delectus nemo accusantium
+          ex quisquam doloribus inventore nihil perferendis quia iure eos fugit
+          quod est facere?
+        </p>
+      </div>
+    </Link>
+    </>
+  );
+};
 
-export default Blog
+export default Blog;
